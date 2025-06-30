51c940d09d54a76908c82be8ff30a14f5e734371
Sofie Willander Thand <sofie.willander.thand@cag.se>
2025-06-30 15:48:25 +0200

    Update import

--- src/templates/ts.test.js
+++ src/templates/ts.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@/test/utils";
-import { COMPONENT_NAME } from "./COMPONENT_NAME";
+import COMPONENT_NAME from "./COMPONENT_NAME";
 
 it("renders", () => {
   render(<COMPONENT_NAME />);
